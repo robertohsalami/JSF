73b23beee03888338ffc381020b6375a7fa28711
Roberto Henrique Salami <roberto.salami@castgroup.com.br>
2018-12-05 16:29:02 -0200

    Finalização da Aula 4 do Curso Java e JSF I: Sua aplicação web com JSF2

--- jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,14 +1,32 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.modelo.Autor;
 import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
 
 	public Livro getLivro() {
 		return livro;
@@ -16,12 +34,33 @@ public class LivroBean {
 
 	public void gravar() {
 		System.out.println("Gravando Livro: " + this.livro.getTitulo());
-		
-		if(livro.getAutores().isEmpty()) {
+
+		if (livro.getAutores().isEmpty()) {
 			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
 		}
-		
+
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		
+		this.livro = new Livro();
+	}
+
+	public List<Autor> getAutores() {
+		return new DAO<Autor>(Autor.class).listaTodos();
+	}
+	
+	public void gravarAutor() {
+		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autor);
+		System.out.println("Livro escrito por: " + autor.getNome()); 
+	}
+	
+	public List<Autor> getAutoresDoLivro(){
+		return this.livro.getAutores();
+	}
+	
+	public List<Livro> getLivros(){
+		List<Livro> listaTodos = new DAO<Livro>(Livro.class).listaTodos();
+		return listaTodos;
 	}
 
 }
