f0626794aa746cb75b24911e277ab7c497393a92
Roberto Henrique Salami <roberto.salami@castgroup.com.br>
2018-12-05 17:58:23 -0200

    Inicio da Aula 4 do Curso Java e JSF I: Sua aplicação web com JSF2

--- jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -36,7 +40,10 @@ public class LivroBean {
 		System.out.println("Gravando Livro: " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			//throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			//Obtemos uma referência do contexto JSF no momento da chamada do método através de FacesContext.getCurrentInstance()
+			FacesContext.getCurrentInstance().addMessage("autor", new FacesMessage("Livro deve ter pelo menos um Autor"));
+			return;
 		}
 
 		new DAO<Livro>(Livro.class).adiciona(this.livro);
@@ -61,6 +68,13 @@ public class LivroBean {
 	public List<Livro> getLivros(){
 		List<Livro> listaTodos = new DAO<Livro>(Livro.class).listaTodos();
 		return listaTodos;
+	}
+	
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException{
+		String valor = value.toString();
+		if(!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
 	}
 
 }
