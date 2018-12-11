990903ebcc81d5d0ec4dcbef2fdb92b9a96176fe
Roberto Henrique Salami <roberto.salami@castgroup.com.br>
2018-12-11 16:47:08 -0200

    Finalização do Curso Java e JSF I: Sua aplicação web com JSF2

--- jsf-livraria/src/br/com/caelum/livraria/bean/LoginBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/LoginBean.java
@@ -1,5 +1,6 @@
 package br.com.caelum.livraria.bean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -20,15 +21,24 @@ public class LoginBean {
 	public String efetuaLogin() {
 		System.out.println("fazendo login: " + this.usuario.getEmail());
 		
+		FacesContext context = FacesContext.getCurrentInstance();
 		boolean existe = new UsuarioDao().existe(this.usuario);
 		if(existe ) {
-			FacesContext context = FacesContext.getCurrentInstance();
 			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
 			return "livro?faces-redirect=true";			
 		}
 		
-		return null;
+		//escopo de flash pra manter mensagem de validação no outro request, dura 2 request o escopo de flash
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("Usuário não encontrado"));
+		return "login?faces-redirect=true";
 		
+	}
+	
+	public String logout() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		return "login?faces-redirect=true";
 	}
 		
 }
