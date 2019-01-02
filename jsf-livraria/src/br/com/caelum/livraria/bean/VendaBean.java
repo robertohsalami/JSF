4385d3aefc143d4d21c984e41f41a1c33724f664
Roberto Henrique Salami <roberto.salami@castgroup.com.br>
2019-01-02 15:22:55 -0200

    Final Curso Java e JSF2 - II

--- jsf-livraria/src/br/com/caelum/livraria/bean/VendaBean.java
+++ jsf-livraria/src/br/com/caelum/livraria/bean/VendaBean.java
@@ -20,26 +20,36 @@ public class VendaBean {
 
 	public BarChartModel getVendasModel() {
 		BarChartModel model = new BarChartModel();
+		model.setAnimate(true);
 
 		ChartSeries vendaSerie = new ChartSeries();
 		vendaSerie.setLabel("Vendas 2018");
 
-		List<Venda> vendas = getVendas();
+		List<Venda> vendas = getVendas(1234);
 		for (Venda venda : vendas) {
 			vendaSerie.set(venda.getLivro().getTitulo(), venda.getQuantidade());
 		}
+		
+		ChartSeries vendaSerie2017 = new ChartSeries();
+		vendaSerie2017.setLabel("Vendas 2017");
+		
+		vendas = getVendas(4321);
+		for (Venda venda : vendas) {
+			vendaSerie2017.set(venda.getLivro().getTitulo(), venda.getQuantidade());
+		}
 
 		model.addSeries(vendaSerie);
+		model.addSeries(vendaSerie2017);
 
 		return model;
 	}
 
-	public List<Venda> getVendas() {
+	public List<Venda> getVendas(long seed) {
 
 		List<Livro> livros = new DAO<Livro>(Livro.class).listaTodos();
 		List<Venda> vendas = new ArrayList<Venda>();
 
-		Random random = new Random(1234);
+		Random random = new Random(seed);
 		for (Livro livro : livros) {
 			Integer quantidade = random.nextInt(500);
 
